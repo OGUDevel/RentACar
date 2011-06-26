245f13f99e08a67a248276efedfa97350fd66d7e
Firat Koyuncu <fkdesigner@hotmail.com>
2011-06-26 21:33:56 +0300

    ilan_sil function has been uploaded.

--- Admin_Methods/ilan_sil.php
+++ Admin_Methods/ilan_sil.php
@@ -1,15 +1,13 @@
 <?php
 function ilan_sil(){
-        // Abi allaskina nedir bu 'id' nereden gelecek nereye gidecek comment duserseniz iyi olur diye
-        // dusunuyorum. Yanlis miyim ? Ve bu arada burda deyimsel hatalar cikiyor gormek istiyorsaniz netbeans kullanıp
-        // readme.md dosyasinda yazdigim gibi 5.3 php framework kullanin.
-	static $ilan_id = $_GET['id'];
-	$ilan_sil = mysql_query ("DELETE FROM cars WHERE id='$ilan_id'");
-	if ($ilan_sil){
-	echo "<font face='verdana' size='2' color='green'>Ilan basariyla silindi.</font>";
+    static $ilan_id = $_GET['id'];
+    $ilan_sil = mysql_query ("DELETE FROM cars WHERE id='$ilan_id'");
+    $ilan_resim_sil = mysql_query ("DELETE FROM pictures WHERE owner_id='$ilan_id'");
+	if ($ilan_sil || $ilan_resim_sil){
+	echo "<b><font face='verdana' size='2' color='green'>Ilan basariyla silindi.</font></b>";
 	}
 	else {
-	echo "<font face='verdana' size='2' color='red'>Ilan silinemedi</font>";
+	echo "<b><font face='verdana' size='2' color='red'>Ilan silinemedi</font></b>";
 	}
 }
 ?>
