e6f5a1ea0ff2a36565fb0ec54bde69ef0f480079
Mahmut Bulut <scienceblock@gmail.com>
2011-07-08 11:46:55 +0300

    .gitignore file

--- adminMethods/getAllAdvert.php
+++ adminMethods/getAllAdvert.php
@@ -2,7 +2,7 @@
 <?php
 function get_all_advert(){
 	static $ilan_id = $_GET['id'];
-	$query = "SELECT * FROM dbName"
+	$query = "SELECT * FROM cars"
 	$get_all = mysql_query($query);
 	if($get_all != NULL){
 		$idfromtable = mysql_fetch_array($get_all, MYSQL_BOTH);
